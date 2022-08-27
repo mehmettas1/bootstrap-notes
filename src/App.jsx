1025fa5229c7033a835db671f0807a569c16f681
mehmet <tas349873@gmail.com>
2022-08-27 23:37:25 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Home from "./pages/Home";
-
+import "./home.css"
 function App() {
   return <div className="App">
     <Home/>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="container">
+      <div className="home-page">
+        <div className="home-page-left"></div>
+        <div className="home-page-right"></div>
+      </div>
+    </div>
   )
 }
 
